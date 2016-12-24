9b1f8d00d41b6bbc70ef6294221d3b0bbcf74377
vagrant <vagrant@precise32.(none)>
2016-12-24 05:10:01 +0000

    completed level 2

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 import requests
 
 app = Flask(__name__)
+app.config["DEBUG"] = True  # Only include this while you are testing your app
 
 @app.route("/")
 def hello():
@@ -9,17 +10,11 @@ def hello():
 
 @app.route("/name")
 def name():
-  return "Joseph Shepley"
-
-@app.route("/website")
-def website():
-  return "http://www.josephshepley.com/"
-
+	return "Your Name"
 
 @app.route("/search")
 def search():
-    return render_template("search.html")
+	return render_template("search.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
