833ba09ce3948385c65ed02122a408107f934cc0
Joe Shepley <jshepley14@gmail.com>
2016-12-25 15:16:25 -0500

    added a second button and page called searchtime

--- app.py
+++ app.py
@@ -21,5 +21,16 @@ def search():
     else: # request.method == "GET"
         return render_template("search.html")
 
+
+
+@app.route("/searchtime", methods=["POST", "GET"])
+def searchtime():
+    if request.method == "POST":
+        url = "https://www.googleapis.com/books/v1/volumes?q=" + request.form["user_search"]
+        response_dict = requests.get(url).json()
+        return render_template("results.html", api_data=response_dict)
+    else: # request.method == "GET"
+        return render_template("searchtime.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
